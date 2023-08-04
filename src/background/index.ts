cefe6744e0443107c0575fdb6dd174eb27289ead
fentosv <fentos.dev@gmail.com>
2023-08-04 16:31:43 +0200

    Reestructure some Chrome API methods

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,12 @@
-import onInstall from './onInstall'
-import onStorageChanged from './onStorageChanged'
+import onInstall from '../utils/onInstall'
+import onMessage from '../utils/onMessage'
+import onStorageChanged from '../utils/onStorageChanged'
 
 console.log('Service worker started.')
 
 onInstall()
 onStorageChanged()
+onMessage()
 
 // Chrome onStartup example
 chrome.runtime.onStartup.addListener(() => {
